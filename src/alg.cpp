1a7ef9965c6536614ea6a14e3883d3553f365147
ApTuKJIb <78952964+ApTuKJIb@users.noreply.github.com>
2021-04-04 11:03:37 +0300

    Бартиев Артур

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  
+  int a = 0, left = 0, right = size - 1;
+  while (left < right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] < value)
+      left = mid + 1;
+    else
+      right = mid;
+  }
+  while (arr[left] == value) {
+    a++;
+    left++;
+  }
+  if (a != 0)
+    return a;
+  else
 return 0;
 }
